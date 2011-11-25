1362b02d1fd89ffe4a9c39473173fe10d4e78b84
Ubuntu <ubuntu@domU-12-31-38-04-6D-D7.compute-1.internal>
2011-11-25 20:40:24 +0000

    .

--- git.rb
+++ git.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 require 'json'
+require 'config.rb'
 
 post '/' do
   key = params[:key]
   if key == 'bGbfQKrMrbqaDdSkCiYYqlYwfdcNMAtn'
-    project_dir = '/home/ubuntu/'
-
     request = JSON.parse(params[:payload])
     project = request['repository']['name']
-    
-    out = `(cd #{project_dir}#{project} && git pull)`
-    puts out
+    puts "Updating " + project
+    system('cd ' + $projects[project] + " && git pull")
   end
 end
